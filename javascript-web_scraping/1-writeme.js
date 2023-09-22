e723e689a914e273be06e9b61f109177fd0ad42c
AdamDiallo <adamdiallo2k@gmail.com>
2023-09-22 21:13:06 +0200

    edit

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -9,8 +9,7 @@ const content = process.argv[3];
 // Write the string to the file
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
-      // Print the error object if there's an error
-      console.error(err);
-      return;
-    }
+    // Print the error object if there's an error
+    console.error(err);
+  }
 });
