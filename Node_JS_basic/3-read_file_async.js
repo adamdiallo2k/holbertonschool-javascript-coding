0649fe3638180def51941e7548397b234f3934a0
Adam <adamdiallo2k@gmail.com>
2023-10-13 13:55:04 -0700

    task 1

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,4 +1,42 @@
-export default function getStudentIdsSum(studentsList) {
-  const totalIdSum = studentsList.reduce((sum, student) => sum + student.id, 0);
-  return totalIdSum;
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      } else {
+        // Divides the data string into an array of rows
+        const lines = data.split('\n').filter((line) => line.trim() !== '');
+        // Browse each row in the lines array (map) and divide it into an array (split)
+        const allData = lines.map((line) => line.split(','));
+        // Cut header line
+        const studentData = allData.slice(1);
+
+        // Search right students
+        const csStudents = studentData.filter((student) => student[3] === 'CS');
+        const sweStudents = studentData.filter((student) => student[3] === 'SWE');
+
+        // Search students names
+        const csStudentsNames = csStudents.map((student) => student[0]);
+        const sweStudentsNames = sweStudents.map((student) => student[0]);
+
+        const result = {
+          totalStudents: studentData.length,
+          csStudentsCount: csStudents.length,
+          csStudentsList: csStudentsNames,
+          sweStudentsCount: sweStudents.length,
+          sweStudentsList: sweStudentsNames,
+        };
+
+        console.log(`Number of students: ${studentData.length}`);
+        console.log(`Number of students in CS: ${csStudents.length}. List: ${csStudentsNames.join(', ')}`);
+        console.log(`Number of students in SWE: ${sweStudents.length}. List: ${sweStudentsNames.join(', ')}`);
+
+        resolve(result);
+      }
+    });
+  });
 }
+
+module.exports = countStudents;
