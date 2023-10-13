851b05c80f2d03f3fff69cc9c04017fa29281526
Adam <adamdiallo2k@gmail.com>
2023-10-13 13:53:42 -0700

    task 1

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,4 +1,42 @@
-export default function getStudentsByLocation(studentsList, city) {
-  const studentsInCity = studentsList.filter((student) => student.location === city);
-  return studentsInCity;
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+  const data = fs.readFileSync(path, 'utf-8');
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+  const students = [];
+  const csStudents = [];
+  const sweStudents = [];
+  const listCS = [];
+  const listSWE = [];
+  let resultCS = '';
+  let resultSWE = '';
+
+  for (const line of lines.slice(1)) { // ignore first row
+      const [firstName, lastName, age, field] = line.split(','); // Column
+      const student = {
+        firstName, lastName, age: parseInt(age, 10), field,
+      }; // create object student
+
+      students.push(student); // Add to array
+
+      if (field === 'CS') {
+        csStudents.push(student);
+        listCS.push(student.firstName);
+        resultCS = listCS.join(', ');
+      } else if (field === 'SWE') {
+          sweStudents.push(student);
+          listSWE.push(student.firstName);
+          resultSWE = listSWE.join(', ');
+      }
+    }
+      console.log(`Number of students: ${students.length}`);
+      console.log(`Number of students in CS: ${csStudents.length}. List: ${resultCS}`);
+      console.log(`Number of students in SWE: ${sweStudents.length}. List: ${resultSWE}`);
+  } catch (err) {
+      throw new Error('Cannot load the database');
+    }
 }
+
+module.exports = countStudents;
